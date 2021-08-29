07c8118fc45fbe9bf8c210c0d1326e574a89508a
rarafy <i.rarafy@gmail.com>
2021-08-29 20:29:28 +0900

    Projectをアップロード

---
+++ _Project/Assets/Reference.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 
+ * ・rosmarinus/jtalkdll｜GitHub
+ * https://github.com/rosmarinus/jtalkdll
+ * 
+ * ・【Unity】using System.Windows.Formsを利用する（Windowsモジュール限定）｜Alpaca Tech Blog
+ * https://alpacatech.hateblo.jp/entry/2018/07/25/012656
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
